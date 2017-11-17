6473ebec18301a467092f0bfc80f63986a24dd74
LastPilot <kristenmarenco@gmail.com>
2017-11-17 14:44:01 -0800

    updating css

---
+++ src/wumbo/controller/AdminHelper.java
@@ -0,0 +1,22 @@
+package wumbo.controller;
+
+public class AdminHelper {
+	
+	public static boolean checkPrereq(String code){
+		return false;
+	}
+	
+	public static String[] getPrereq(String code){
+		return null;
+	}
+	
+	public static void mapPrereq(){
+		
+		
+		
+		
+	}
+	
+	
+	
+}
